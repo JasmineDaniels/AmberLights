761fa94b44a900821b1e0b0b0484adb6de86dc84
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-07-25 02:31:32 -0400

    add classes

---
+++ db/update.sql
@@ -0,0 +1,6 @@
+update employee
+set first_name = 
+set last_name =
+set role =
+set employee_manager = 
+where id = ?
---
+++ server.js
@@ -0,0 +1,10 @@
+//const inquirer = require('inquirer')
+const mysql = require('mysql2')
+
+
+// start server
+
+
+
+
+// start inquirer menu 
