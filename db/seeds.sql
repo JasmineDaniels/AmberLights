b74dc6d6b78af6d48e612333d12ff1f73402757c
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-07-26 23:36:58 -0400

    secured connection + select queries -- wip

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,22 @@
-insert into department (dept_name)
+insert into departments (dept_name)
 values  ("Sales"),
         ("Engineering"),
         ("Marketing");
 
 
-insert into role (title, salary, department_id)
-values  ()
+insert into role (title, salary, department)
+values  ("Sales Executive", 130000, 1 ),
+        ("Salesperson", 80000, 1 ),
+        ("Tech Lead", 180000, 2 ),
+        ("Software Engineer", 120000, 2 ),
+        ("Project Manager", 140000, 3 ),
+        ("Promotions Assistant", 70000, 3 );
+
+
+-- insert into manager (first_name, last_name, role)
+-- values  ("Sean", "Brown", "Sales Executive"),
+--         ("Mya", "Vasquez", "Salesperson"),
+--         ("Tao", "Jin", "Tech Lead"),
+--         ("Ryan", "Hughes", "Software Engineer"),
+--         ("Tanya", "Pratt", "Project Manager"),
+--         ("Sabrina", "Dunn", "Promotions Assistant"),
--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cTable = require('console.table')
 //const main_menu = require('./prompts/main-menu')
+//const table = cTable.getTable()
 
 const PORT = process.env.PORT || 5001;
 const app = express();
 
 //middlewear
 app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+// static for 
 
 // start server
-const db = mysql.createConnection({
+const db = mysql.createConnection({ //returns a js object 
     host: 'localhost',
     user: 'root',
     password: 'docker',
     database: 'employee_tracker'
 });
-  
 
+//Get All Depeartments
+db.query('Select * from departments', (err, data) => { // if data JSON.stringify(data) res.json(data)
+    err ? console.error(err) : console.table(data)
+});
+
+//Get All Roles
+db.query('Select * from role', (err, data) => { // if data JSON.stringify(data) res.json(data)
+    err ? console.error(err) : console.table(data)
+});
 
 
 // start inquirer menu 
